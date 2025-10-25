e7617464e291bb61a11e770e685f6d499c63d1a8
ducky1129 <schober1129@gmail.com>
2025-10-24 23:26:09 -0500

    lkj

---
+++ gameScheduleScraper.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+
+headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Cache-Control": "max-age=0"
+}
+
+
+URLS = ['https://www.basketball-reference.com/boxscores/202504190IND.html', 'https://www.basketball-reference.com/boxscores/202504190DEN.html', 'https://www.basketball-reference.com/boxscores/202504190DEN.html', 'https://www.basketball-reference.com/boxscores/202504190NYK.html',
+ 'https://www.basketball-reference.com/boxscores/202504190LAL.html', 'https://www.basketball-reference.com/boxscores/202504200OKC.html',
+ 'https://www.basketball-reference.com/boxscores/202504200BOS.html', 'https://www.basketball-reference.com/boxscores/202504200CLE.html',
+ 'https://www.basketball-reference.com/boxscores/202504200HOU.html', 'https://www.basketball-reference.com/boxscores/202504210NYK.html',
+ 'https://www.basketball-reference.com/boxscores/202504210DEN.html', 'https://www.basketball-reference.com/boxscores/202504220IND.html',
+ 'https://www.basketball-reference.com/boxscores/202504220OKC.html', 'https://www.basketball-reference.com/boxscores/202504220LAL.html',
+ 'https://www.basketball-reference.com/boxscores/202504230BOS.html', 'https://www.basketball-reference.com/boxscores/202504230CLE.html',
+ 'https://www.basketball-reference.com/boxscores/202504230HOU.html', 'https://www.basketball-reference.com/boxscores/202504240DET.html',
+ 'https://www.basketball-reference.com/boxscores/202504240MEM.html', 'https://www.basketball-reference.com/boxscores/202504240LAC.html',
+ 'https://www.basketball-reference.com/boxscores/202504250ORL.html', 'https://www.basketball-reference.com/boxscores/202504250MIL.html',
+ 'https://www.basketball-reference.com/boxscores/202504250MIN.html', 'https://www.basketball-reference.com/boxscores/202504260MIA.html',
+ 'https://www.basketball-reference.com/boxscores/202504260MEM.html', 'https://www.basketball-reference.com/boxscores/202504260LAC.html',
+ 'https://www.basketball-reference.com/boxscores/202504260GSW.html', 'https://www.basketball-reference.com/boxscores/202504270DET.html',
+ 'https://www.basketball-reference.com/boxscores/202504270MIN.html', 'https://www.basketball-reference.com/boxscores/202504270ORL.html',
+ 'https://www.basketball-reference.com/boxscores/202504270MIL.html', 'https://www.basketball-reference.com/boxscores/202504280MIA.html',
+ 'https://www.basketball-reference.com/boxscores/202504280GSW.html', 'https://www.basketball-reference.com/boxscores/202504290IND.html',
+ 'https://www.basketball-reference.com/boxscores/202504290NYK.html', 'https://www.basketball-reference.com/boxscores/202504290BOS.html',
+ 'https://www.basketball-reference.com/boxscores/202504290DEN.html', 'https://www.basketball-reference.com/boxscores/202504300HOU.html',
+ 'https://www.basketball-reference.com/boxscores/202504300LAL.html']
+
+file = open('gameTable.txt', 'w')
+
+file.write(f"""
+    CREATE TABLE Game (
+        Game_ID INT NOT NULL PRIMARY KEY,
+        Date varchar(20)
+);
+
+    CREATE TABLE Schedule (
+        Game_ID INT NOT NULL PRIMARY KEY,
+        Team_Code char(3)
+);
+    """)
+counter = 0
+for url in URLS:
+    try:
+        counter += 1
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Each team name appears inside <strong><a> tags within divs with class 'scorebox'
+        scorebox = soup.find("div", class_="scorebox")
+        team_links = scorebox.find_all("a", href=re.compile(r"^/teams/([A-Z]{3})/"))
+        print(team_links)
+        time.sleep(5)
+
+        date = soup.find('div', class_='scorebox_meta')
+        date = date.find_all("div")[0].strip()
+
+        file.write(f'INSERT INTO Schedule (Game_ID, Date) VALUES ({counter}, {date});\n'
+                   f'INSERT INTO Game (Game_ID, Date) VALUES ({counter}, {team_links[0]});\n)'
+                   f'INSERT INTO Game (Game_ID, Date) VALUES ({counter}, {team_links[1]});\n)')
+
+
+    except Exception as e:
+        print(e)
